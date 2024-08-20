23370e89b169da412cae0b86c88f5b4d2e5e5226
Dalaqa25 <g.dalaqishvili01@gmail.com>
2024-08-20 21:05:20 +0400

    IsPalindrome 125

--- obj/Debug/net8.0/LeetCodesProblems.AssemblyInfo.cs
+++ obj/Debug/net8.0/LeetCodesProblems.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LeetCodesProblems")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9a169dc7c3565bf25ece6579fb77339bbf5d949")]
 [assembly: System.Reflection.AssemblyProductAttribute("LeetCodesProblems")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LeetCodesProblems")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
