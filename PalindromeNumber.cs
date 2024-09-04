e17d61e3af33e108fda6a17109723c2b9f19d945
Dalaqa25 <g.dalaqishvili01@gmail.com>
2024-09-04 08:36:41 +0400

    Plaindrome number

---
+++ PalindromeNumber.cs
@@ -0,0 +1,31 @@
+﻿namespace LeetCodeProblems;
+
+public class PalindromeNumber
+{
+    public bool IsPalindrome(int x)
+    {
+        // Negative numbers are not palindromes
+        if (x < 0) return false;
+        
+        //convert to string
+        string s = x.ToString();
+        
+        // set pointers
+        int left = 0;
+        int right = s.Length - 1;
+        
+        
+        while (left < right)
+        {
+            // If they are aren't 
+            if (s[left] != s[right])
+                return false;
+            
+            // If they are
+            left++;
+            right++;
+        }
+
+        return true;
+    }
+}
