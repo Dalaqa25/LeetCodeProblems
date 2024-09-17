fbaf607f766a67e0709f08dae716067042b76ece
dalaqa25 <g.dalaqishvili01@gmail.com>
2024-09-17 19:50:51 +0400

    group anagrams(my way)

---
+++ GroupAnagrams.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Soluti {
+    public List<IList<string>> GroupAnagrams(string[] strs) 
+    {
+        // Step 1: Create the dictionary to group anagrams
+        Dictionary<string, List<string>> dict = new Dictionary<string, List<string>>();
+
+        // Step 2: Process each string
+        foreach (string str in strs)
+        {
+            // Convert to character array and sort
+            char[] charArray = str.ToCharArray();
+            Array.Sort(charArray);
+            string sortedStr = new string(charArray);
+
+            // Add the original string to the dictionary
+            if (dict.ContainsKey(sortedStr))
+            {
+                dict[sortedStr].Add(str);
+            }
+            else
+            {
+                dict[sortedStr] = new List<string> { str };
+            }
+        }
+
+        // Step 3: Return the grouped anagrams
+        return dict.Values.ToList<IList<string>>();
+    }
+}
--- obj/Debug/net8.0/LeetCodeProblems.AssemblyInfo.cs
+++ obj/Debug/net8.0/LeetCodeProblems.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LeetCodeProblems")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+509782f1eefd44957ac89b03f9c94ac345175d6e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abfe00cd1f01610af8a85a5cb0f5d717ef3bb2b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("LeetCodeProblems")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LeetCodeProblems")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
