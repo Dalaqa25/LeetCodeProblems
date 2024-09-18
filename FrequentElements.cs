66954ca685b0138fdb5bf0cde66045928eb61984
dalaqa25 <g.dalaqishvili01@gmail.com>
2024-09-18 19:53:19 +0400

    frequent numbers

---
+++ FrequentElements.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCodeProblems;
+
+public class FrequentElements
+{
+    public int[] TopKFrequent(int[] nums, int k)
+    {
+        Dictionary<int, int> dict = new Dictionary<int, int>();
+
+        foreach (var num in nums)
+        {
+            if (dict.ContainsKey(num))
+            {
+                dict[num]++;
+            }
+            else
+            {
+                dict[num] = 1;
+            }
+        }
+        
+        //set k 
+        k = dict.Values.Max();
+        
+        //find frequent numbers
+        var mostFrequentElements = dict
+            .Where(x => x.Value == k)
+            .Select(x => x.Key)
+            .ToList();
+            
+      
+    }
+}
